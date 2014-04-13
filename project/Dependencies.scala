96c4cdf101d75a017ff10c7b8ccb9d0f270c2deb
manboubird <manboubird@gmail.com>
2014-04-13 23:14:27 +0900

    bump up to sbt 13.1, scala 2.10.3 and scalding 0.9.1 with CDH mr1-cdh4.3.0.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -15,21 +15,25 @@ import sbt._
 object Dependencies {
   val resolutionRepos = Seq(
     "ScalaTools snapshots at Sonatype" at "https://oss.sonatype.org/content/repositories/snapshots/",
-    "Concurrent Maven Repo" at "http://conjars.org/repo" // For Scalding, Cascading etc
+    "Concurrent Maven Repo" at "http://conjars.org/repo", // For Scalding, Cascading etc
+    "cloudera-releases" at "https://repository.cloudera.com/artifactory/cloudera-repos"
   )
 
   object V {
-    val scalding  = "0.9.1"
-    val hadoop    = "1.1.2"
-    val specs2    = "2.3.11"
+    val scalding  = "0.9.1-mr1-cdh4.3.0"
+    val hadoopCore    = "2.0.0-mr1-cdh4.3.0"
+    val hadoopCommon = "2.0.0-cdh4.3.0"
+    val hadoopClient = "2.0.0-mr1-cdh4.3.0"
+    val specs2    = "2.3.11"//"1.12.3" // -> "1.13" when we bump to Scala 2.10.0
     // Add versions for your additional libraries here...
   }
 
   object Libraries {
-    val scaldingCore = "com.twitter"                %%  "scalding-core"       % V.scalding
-    val hadoopCore   = "org.apache.hadoop"          %   "hadoop-core"         % V.hadoop       % "provided"
+    val scaldingCore = "com.twitter"                %% "scalding-core"        % V.scalding
+    val hadoopCore   = "org.apache.hadoop"          %  "hadoop-core"          % V.hadoopCore   % "provided"
     // Add additional libraries from mvnrepository.com (SBT syntax) here...
-
+    val hadoopCommon = "org.apache.hadoop"          %  "hadoop-common"        % V.hadoopCommon % "provided"
+    val hadoopClient = "org.apache.hadoop"          %  "hadoop-client"        % V.hadoopClient % "provided"
     // Scala (test only)
     val specs2       = "org.specs2"                 %% "specs2"               % V.specs2       % "test"
   }
